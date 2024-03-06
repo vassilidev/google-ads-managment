59102be9a5299f0ab5c0abfdf870c3f6e88a8548
vassilidev <vassilidevnet@gmail.com>
2024-03-06 11:40:25 +0100

    feat: add schedule

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::fallback(function () {
     return to_route('filament.admin.auth.login');
 });
+
+Route::get('startCron', static function () {
+    activity('cron')->log('Start the CRON ' . now());
+
+    Artisan::call('schedule:run');
+    Artisan::call('queue:work --stop-when-empty --queue=high,default');
+
+    activity('cron')->log('End the CRON ' . now());
+
+    return response('', 200);
+});
