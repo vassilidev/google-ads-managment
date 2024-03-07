045ccd9664769b1d4a8f8f91d85e16e970d81cc0
vassilidev <vassilidevnet@gmail.com>
2024-03-07 11:12:39 +0100

    fix: change kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('telescope:prune --hours=72')->daily();
-        $schedule->command(FetchGoogleAdsCampaign::class)->everyFiveMinutes();
+        $schedule->command(FetchGoogleAdsCampaign::class)->everyFifteenMinutes();
     }
 
     /**
